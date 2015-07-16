a97d8b1f56c4a36db0f9d41ad05e6ec344024796
Brian Grohe <grohe43@gmail.com>
2015-07-16 15:08:09 -0400

    Removed lorem ipsum library
    The library was removed in django 1.8

--- mailviews/tests/emails/previews.py
+++ mailviews/tests/emails/previews.py
@@ -1,7 +1,6 @@
 import random
 
 from django import forms
-from django.contrib.webdesign.lorem_ipsum import paragraphs, words
 
 from mailviews.previews import Preview, site
 from mailviews.tests.emails.views import (BasicEmailMessageView,
@@ -14,8 +13,23 @@ class BasicPreview(Preview):
     description = 'A basic text email message.'
 
     def get_message_view(self, request):
-        subject = words(random.randint(5, 20), common=False)
-        content = '\n'.join(paragraphs(random.randint(3, 6)))
+        subject = ('''Robot ipsum datus scan amet, constructor ad ut splicing 
+         elit, sed do errus mod tempor in conduit ut laboratory et deplore
+         electromagna aliqua. Ut enim ad minimum veniam, quis no indestruct
+         exoform ullamco laboris nisi ut alius equip ex ea commando evaluant.
+         Duis ex machina aute ire dolorus in scan detectus an voluptate volt
+         esse cesium dolore eu futile nulla parameter. Execute primus sint
+         occaecat cupidatat non proident, sunt in culpa qui technia deserunt
+         mondus anim id est proceus.''')
+        content = ('''\n Robot ipsum Calciolite misoplex teloene xanthphobe
+          ossidrome auriculomania sexiatric Arabobiosis bartomy. Warcele
+          heliotherm holophobic hematocele exbitropic gynase septuaphage
+          yobiiatrics. Luteoscript underscopy xeroese homeometry
+          parthenophilous gynotropy homolatry hendecaales. Bathourous
+          aleuroeaux basidom egoably Indoemia parilogue Araboic. Indoathon
+          selenogate spirohood hieroistical gastrograph autcentric butyroeaux
+          trisity combioideae myescent. Mangamous sphagia spondylopara
+          psychroplasia arnophilia adenorama ventrigene leptomorphic.''')
         return self.message_view(subject, content)
 
 
